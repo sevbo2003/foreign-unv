bbb0cb2ab420388e2cfc565f7ae521cdd6f3509b
justSevbo <sevbofx@gmail.com>
2022-02-13 18:23:53 +0500

    New updates

--- university/views.py
+++ university/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import University, Category, Tags
 
 
 def university_list(request):
-    return render(request, 'home.html')
+    universities = University.objects.all()
+    sponsors = University.objects.filter(sponsor=True)
+    categories = Category.objects.all()
+    tags = Tags.objects.all()
+    context = {
+        'univers': universities,
+        'sponsors': sponsors,
+        'categories': categories,
+        'tags': tags
+    }
+    return render(request, 'home.html', context)
