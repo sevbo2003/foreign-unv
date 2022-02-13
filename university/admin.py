0ad11c1b4cd8b256f923ee5ef6f00fa39935c2ff
justSevbo <sevbofx@gmail.com>
2022-02-13 17:07:28 +0500

    Admin panel created

--- university/admin.py
+++ university/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Davlat, Category, Tags, Author, Fanlar, University
 
-# Register your models here.
+admin.site.register(Davlat)
+admin.site.register(Category)
+admin.site.register(Tags)
+admin.site.register(Author)
+admin.site.register(Fanlar)
+
+
+@admin.register(University)
+class UniverAdmin(admin.ModelAdmin):
+    list_display = ('qisqa_nomi', 'location', 'davlat', 'author',
+                    'website', 'category', 'imtihon_sanasi', 'sponsor')
+    search_fields = ('qisqa_nomi', 'universitet', 'location', 'davlat', 'author',
+                     'website', 'category',)
+    list_filter = ('author', 'davlat', 'location',)
+    prepopulated_fields = {'slug': ('universitet',)}
