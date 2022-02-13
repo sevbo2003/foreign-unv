6b28a5035f329bf62d40543f790b2ff685e78a65
justSevbo <sevbofx@gmail.com>
2022-02-13 18:50:05 +0500

    Search method added

--- university/views.py
+++ university/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import University, Category, Tags
+from django.db.models import Q
 
 
 def university_list(request):
-    universities = University.objects.all()
+    search_univer = request.GET.get('search')
+    if search_univer:
+        universities = University.objects.filter(Q(universitet__icontains=search_univer) & Q(about__icontains=search_univer))
+    else:
+        universities = University.objects.all()
     sponsors = University.objects.filter(sponsor=True)
     categories = Category.objects.all()
     tags = Tags.objects.all()
